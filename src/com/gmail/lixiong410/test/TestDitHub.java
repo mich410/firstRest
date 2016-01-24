5b3f4cb5491f307ae414b689886087468affb7b6
mich410 <lixiong410@gmail.com>
2016-01-24 17:56:09 +0800

    submit changes into github!
    Change-Id: Ica2c4eaaef9811acd3559e017c448187dcf307cc
    Signed-off-by: mich410 <lixiong410@gmail.com>

--- src/com/gmail/lixiong410/test/TestDitHub.java
+++ src/com/gmail/lixiong410/test/TestDitHub.java
@@ -3,6 +3,7 @@ package com.gmail.lixiong410.test;
 public class TestDitHub {
 	public static void main(String[] args){
 		System.out.println("hello,github,hahaha!!!");
+		System.out.println("my second time to submit code to github,hahaha!!!");
 	}
 
 }
